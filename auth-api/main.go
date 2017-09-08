f6e6570b5c31025f06120a3d91203be4a91ef68a
elgris <iddqd07@yandex.ru>
2017-09-08 18:27:18 +0200

    Adds some JWT ops between Auth API and UsersAPI

--- auth-api/main.go
+++ auth-api/main.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"log"
 	"net/http"
+	"os"
 	"time"
 
 	jwt "github.com/dgrijalva/jwt-go"
@@ -22,6 +23,18 @@ var (
 )
 
 func main() {
+	port := os.Getenv("AUTH_API_PORT")
+	userAPIAddress := os.Getenv("USER_API_ADDRESS")
+
+	userService := UserService{
+		UserAPIAddress: userAPIAddress,
+		AllowedUserHashes: map[string]interface{}{
+			"admin_admin": nil,
+			"johnd_foo":   nil,
+			"janed_ddd":   nil,
+		},
+	}
+
 	// Echo instance
 	e := echo.New()
 
@@ -35,10 +48,10 @@ func main() {
 		return c.String(http.StatusOK, "Auth API, written in Go\n")
 	})
 
-	e.POST("/login", loginHandler)
+	e.POST("/login", getLoginHandler(userService))
 
 	// Start server
-	e.Logger.Fatal(e.Start(":1323"))
+	e.Logger.Fatal(e.Start(":" + port))
 }
 
 type LoginRequest struct {
@@ -46,40 +59,44 @@ type LoginRequest struct {
 	Password string `json:"password"`
 }
 
-func loginHandler(c echo.Context) error {
-	requestData := LoginRequest{}
-	decoder := json.NewDecoder(c.Request().Body)
-	if err := decoder.Decode(&requestData); err != nil {
-		log.Printf("could not read credentials from POST body: %s", err.Error())
-		return ErrHttpGenericMessage
-	}
-
-	user, err := login(requestData.Username, requestData.Password)
-	if err != nil {
-		if err != ErrWrongCredentials {
-			log.Printf("could not authorize user '%s': %s", requestData.Username, err.Error())
+func getLoginHandler(userService UserService) echo.HandlerFunc {
+	f := func(c echo.Context) error {
+		requestData := LoginRequest{}
+		decoder := json.NewDecoder(c.Request().Body)
+		if err := decoder.Decode(&requestData); err != nil {
+			log.Printf("could not read credentials from POST body: %s", err.Error())
 			return ErrHttpGenericMessage
 		}
 
-		return ErrWrongCredentials
+		user, err := userService.Login(requestData.Username, requestData.Password)
+		if err != nil {
+			if err != ErrWrongCredentials {
+				log.Printf("could not authorize user '%s': %s", requestData.Username, err.Error())
+				return ErrHttpGenericMessage
+			}
+
+			return ErrWrongCredentials
+		}
+		token := jwt.New(jwt.SigningMethodHS256)
+
+		// Set claims
+		claims := token.Claims.(jwt.MapClaims)
+		claims["id"] = user.ID
+		claims["name"] = user.Name
+		claims["role"] = user.Role
+		claims["exp"] = time.Now().Add(time.Hour * 72).Unix()
+
+		// Generate encoded token and send it as response.
+		t, err := token.SignedString([]byte(jwtSecret))
+		if err != nil {
+			log.Printf("could not generate a JWT token: %s", err.Error())
+			return ErrHttpGenericMessage
+		}
+
+		return c.JSON(http.StatusOK, map[string]string{
+			"accessToken": t,
+		})
 	}
-	token := jwt.New(jwt.SigningMethodHS256)
 
-	// Set claims
-	claims := token.Claims.(jwt.MapClaims)
-	claims["id"] = user.ID
-	claims["name"] = user.Name
-	claims["role"] = user.Role
-	claims["exp"] = time.Now().Add(time.Hour * 72).Unix()
-
-	// Generate encoded token and send it as response.
-	t, err := token.SignedString([]byte(jwtSecret))
-	if err != nil {
-		log.Printf("could not generate a JWT token: %s", err.Error())
-		return ErrHttpGenericMessage
-	}
-
-	return c.JSON(http.StatusOK, map[string]string{
-		"accessToken": t,
-	})
+	return echo.HandlerFunc(f)
 }
--- auth-api/user.go
+++ auth-api/user.go
@@ -1,28 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
 
-var allowedUsers = map[string]User{
-	"admin_admin": User{
-		ID:   "1",
-		Name: "admin",
-		Role: "admin",
-	},
-	"peterf_12345": User{
-		ID:   "2",
-		Name: "Peter F",
-		Role: "user",
-	},
-	"johndoe_foofoofoo": User{
-		ID:   "3",
-		Name: "John Doe",
-		Role: "user",
-	},
-	"janed_ddd": User{
-		ID:   "4",
-		Name: "Jane Doe",
-		Role: "user",
-	},
+	jwt "github.com/dgrijalva/jwt-go"
+)
+
+var allowedUserHashes = map[string]interface{}{
+	"admin_admin": nil,
+	"johnd_foo":   nil,
+	"janed_ddd":   nil,
 }
 
 type User struct {
@@ -31,12 +20,55 @@ type User struct {
 	Role string
 }
 
-func login(username, password string) (User, error) {
-	userKey := fmt.Sprintf("%s_%s", username, password)
-	u, ok := allowedUsers[userKey]
-	if !ok {
-		return u, ErrWrongCredentials // this is BAD, business logic layer must not return HTTP-specific errors
+type UserService struct {
+	UserAPIAddress    string
+	AllowedUserHashes map[string]interface{}
+}
+
+func (h *UserService) Login(username, password string) (User, error) {
+
+	user, err := h.getUser(username)
+	if err != nil {
+		return user, err
 	}
 
-	return u, nil
+	userKey := fmt.Sprintf("%s_%s", user.Name, password)
+
+	if _, ok := h.AllowedUserHashes[userKey]; !ok {
+		return user, ErrWrongCredentials // this is BAD, business logic layer must not return HTTP-specific errors
+	}
+
+	return user, nil
+}
+
+func (h *UserService) getUser(username string) (User, error) {
+	var user User
+
+	token, err := h.getUserAPIToken(username)
+	if err != nil {
+		return user, err
+	}
+
+	url := fmt.Sprintf("%s/users/%s", h.UserAPIAddress, username)
+	req, _ := http.NewRequest("GET", url, nil)
+	req.Header.Add("Authorization", "Bearer "+token)
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return user, err
+	}
+
+	defer resp.Body.Close()
+
+	err = json.NewDecoder(resp.Body).Decode(&user)
+
+	return user, err
+}
+
+func (h *UserService) getUserAPIToken(username string) (string, error) {
+	token := jwt.New(jwt.SigningMethodHS256)
+	claims := token.Claims.(jwt.MapClaims)
+	claims["username"] = username
+	claims["scope"] = "read"
+	return token.SignedString([]byte(jwtSecret))
 }
