bc75b97db809373ab83c7b044bd316ed6cd82e6e
elgris <iddqd07@yandex.ru>
2018-02-07 18:51:35 +0100

    Adds zipkin tracing to todos api

--- todos-api/server.js
+++ todos-api/server.js
@@ -2,13 +2,30 @@ const express = require('express')
 const bodyParser = require("body-parser")
 const jwt = require('express-jwt')
 
+const {Tracer, ExplicitContext, BatchRecorder} = require('zipkin');
+const {HttpLogger} = require('zipkin-transport-http');
+const zipkinMiddleware = require('zipkin-instrumentation-express').expressMiddleware;
+
 const routes = require('./routes')
 
+const ZIPKIN_URL = process.env.ZIPKIN_ADDRESS || 'http://127.0.0.1:9411/api/v2/spans';
 const port = process.env.TODO_API_PORT || 8082
 const jwtSecret = process.env.JWT_SECRET || "myfancysecret"
 const app = express()
 
+// tracing
+const ctxImpl = new ExplicitContext();
+const recorder = new  BatchRecorder({
+  logger: new HttpLogger({
+    endpoint: ZIPKIN_URL
+  })
+});
+const localServiceName = 'todos-api';
+const tracer = new Tracer({ctxImpl, recorder, localServiceName});
+
+
 app.use(jwt({ secret: jwtSecret }))
+app.use(zipkinMiddleware({tracer}));
 app.use(function (err, req, res, next) {
   if (err.name === 'UnauthorizedError') {
     res.status(401).send({ message: 'invalid token' })
