e5b495c79c32958b97305c52564fea8ad4f75950
elgris <iddqd07@yandex.ru>
2018-02-16 22:24:59 +0100

    Adds Zipkin processing to log-message-processor

--- log-message-processor/main.py
+++ log-message-processor/main.py
@@ -1,14 +1,57 @@
 import time
 import redis
 import os
+import json
+import requests
+from py_zipkin.zipkin import zipkin_span, ZipkinAttrs
+import time
+import random
 
+def log_message(message):
+    time_delay = random.randrange(0, 2000)
+    time.sleep(time_delay / 1000)
+    print ('message received after waiting for {}ms: {}'.format(time_delay, message))
 
 if __name__ == '__main__':
     redis_host = os.environ['REDIS_HOST']
     redis_port = int(os.environ['REDIS_PORT'])
     redis_channel = os.environ['REDIS_CHANNEL']
+    zipkin_url = os.environ['ZIPKIN_URL'] if 'ZIPKIN_URL' in os.environ else ''
+    def http_transport(encoded_span):
+        requests.post(
+            zipkin_url,
+            data=encoded_span,
+            headers={'Content-Type': 'application/x-thrift'},
+        )
 
     pubsub = redis.Redis(host=redis_host, port=redis_port, db=0).pubsub()
     pubsub.subscribe([redis_channel])
     for item in pubsub.listen():
-        print ('message received : {}'.format(item['data']))
+        try:
+            message = json.loads(str(item['data'].decode("utf-8")))
+        except Exception as e:
+            log_message(e)
+            continue
+
+        if not zipkin_url or 'zipkinSpan' not in message:
+            log_message(message)
+            continue
+
+        span_data = message['zipkinSpan']
+        with zipkin_span(
+            service_name='log-message-processor',
+            zipkin_attrs=ZipkinAttrs(
+                trace_id=span_data['_traceId']['value'],
+                span_id=span_data['_spanId'],
+                parent_span_id=span_data['_parentId']['value'],
+                is_sampled=span_data['_sampled']['value'],
+                flags=None
+            ),
+            span_name='save_log',
+            transport_handler=http_transport,
+            sample_rate=100
+        ):
+            log_message(message)
+
+
+
