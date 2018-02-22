0ba6b0c83f51ac9f5ed742aee13094e8d9b9d731
elgris <iddqd07@yandex.ru>
2018-02-22 14:48:50 +0100

    Makes zipkin setting in frontend consistent with other components

--- frontend/config/index.js
+++ frontend/config/index.js
@@ -37,7 +37,7 @@ module.exports = {
         secure: false
       },
       '/zipkin': {
-        target: process.env.ZIPKIN_ADDRESS || 'http://127.0.0.1:9411/api/v2/spans',
+        target: process.env.ZIPKIN_URL || 'http://127.0.0.1:9411/api/v2/spans',
         pathRewrite: {
           '^/zipkin': ''
         },
